8c98f9d967376868fe00fd33067837bcc6b4cc69
TrustarScientist <89700191+TrustarScientist@users.noreply.github.com>
2021-11-27 21:04:44 +0100

    Update models.py

--- social/models.py
+++ social/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+
 class Niche(models.Model):
     name = models.CharField(max_length=50)
     
-
 class NicheMeta(models.Model):
     niche = models.ForeignKey(Niche, on_delete=models.CASCADE)
     parent = models.ForeginKey(Niche, on_delete=models.CASCADE)
@@ -13,3 +14,7 @@ class NicheFollower(models.Model):
     niche = models.ForeignKey(Niche, on_delete=models.CASCADE, related_name="follower")
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="following")
     started_following = models.DateField(auto_now_add=True)
+
+class NicheMember(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="membership")
+    date = models.DateField(auto_now_add=True)
